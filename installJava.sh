c354d2356af6330e83f496b303d0106bdad615ce
SirKapp'n <42256091+MasterGamingYT@users.noreply.github.com>
2020-07-18 21:26:25 -0500

    Add 32bit support for Java 8

--- installJava.sh
+++ installJava.sh
@@ -11,16 +11,14 @@ exit;;
   * ) echo -e "\e[31mYou have to say y or n, don't play with me smh"
 exit;;
 esac
-
 if [[ -d "/usr/java" ]]
 then
     sudo rm -r /usr/java && sudo mkdir /usr/java
 else 
     sudo mkdir /usr/java
 fi
-
 PS3='Which Java version do you have downloaded?: '
-options=("Java 8 (JDK)" "Java 8 (JRE)" "Java 14" "Java 11")
+options=("Java 8 (JDK)" "Java 8 (JRE)" "Java 8 32bit (JDK)" "Java 8 32bit (JRE)" "Java 8 " "Java 14" "Java 11")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -87,6 +85,45 @@ fi
             sudo rm jdk-11.0.8_linux-x64_bin.tar.gz
             sudo mv jdk-11.0.8 /usr/java
             export JAVA_BIN_DIR=/usr/java/jdk-11.0.8/bin
+            cd ${JAVA_BIN_DIR}
+            a=(java javac javadoc javah javap javaws)
+            for exe in ${a[@]}; do
+            sudo update-alternatives --install "/usr/bin/${exe}" "${exe}" "${JAVA_BIN_DIR}/${exe}" 1
+            sudo update-alternatives --set ${exe} ${JAVA_BIN_DIR}/${exe}
+        done
+            if [ $? -eq 0 ]
+then
+    echo -e "\e[32mInstalled Java successfully, run the command java -version to test the installation."
+else
+    echo -e "\e[31mThe installation failed."
+fi
+            exit
+            ;;
+        "Java 8 32bit (JDK)")
+            sudo tar zxvf jdk-8u261-linux-i586.tar.gz
+            sudo rm jdk-8u261-linux-i586.tar.gz
+            sudo mv jdk1.8.0_261 /usr/java
+            export JAVA_BIN_DIR=/usr/java/jdk1.8.0_261/bin
+            cd ${JAVA_BIN_DIR}
+            a=(java javac javadoc javah javap javaws)
+            for exe in ${a[@]}; do
+            sudo update-alternatives --install "/usr/bin/${exe}" "${exe}" "${JAVA_BIN_DIR}/${exe}" 1
+            sudo update-alternatives --set ${exe} ${JAVA_BIN_DIR}/${exe}
+        done
+            if [ $? -eq 0 ]
+then
+    echo -e "\e[32mInstalled Java successfully, run the command java -version to test the installation."
+else
+    echo -e "\e[31mThe installation failed."
+fi
+            
+            exit
+            ;;
+        "Java 8 32bit (JRE)")
+            sudo tar zxvf jre-8u261-linux-i586.tar.gz
+            sudo rm jre-8u261-linux-i586.tar.gz
+            sudo mv jre1.8.0_261 /usr/java
+            export JAVA_BIN_DIR=/usr/java/jre1.8.0_261/bin
             cd ${JAVA_BIN_DIR}
             a=(java javac javadoc javah javap javaws)
             for exe in ${a[@]}; do
